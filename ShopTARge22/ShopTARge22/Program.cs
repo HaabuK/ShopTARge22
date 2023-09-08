56921bbf4c11e374e68d9e035ce4efb2cb9988c6
HaabuK <karl.h.haabu@gmail.com>
2023-09-08 10:19:10 +0300

    SQLiga peab minema üle dockerisse

--- ShopTARge22/ShopTARge22/Program.cs
+++ ShopTARge22/ShopTARge22/Program.cs
@@ -1,13 +1,14 @@
 ﻿using ShopTARge22.Data;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.SqlServer;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-//builder.Services.AddDbContext<ShopTARge22Context>(options => options.UseSqlServer);
+builder.Services.AddDbContext<ShopTARge22Context>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 
 var app = builder.Build();
 
